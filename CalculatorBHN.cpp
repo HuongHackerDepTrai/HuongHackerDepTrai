32c56ebb13226e6f1325aea28f36a712ca063836
--SuperHacker-- <97739438+HuongHackerDepTrai@users.noreply.github.com>
2022-06-01 20:25:37 +0700

    Update and rename Calculator.cpp to CalculatorBHN.cpp

--- CalculatorBHN.cpp
+++ CalculatorBHN.cpp
@@ -20,8 +20,8 @@ void NormalMath(){
 	system("color f1");
 	double so[255],kq,ns[254];
 	gotoxy(0,5);cout<<"±--±--±--±--±--±--±--±--±--±--±--±";
-	char dau,sl=0;
-	int d[254];
+	char dau;
+	int d[254];d[0]==43;
 	gotoxy(40,1);cout<<"   y_";
 	gotoxy(40,2);cout<<"V: Vx";
 	gotoxy(40,3);cout<<"^: x^y";
@@ -39,15 +39,17 @@ void NormalMath(){
 	{
 		gotoxy(3,2);cout<<"   ";
 		gotoxy(3,2);cin>>dau;d[i-1]=dau;
-		if(dau=='+')	 {gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];kq=kq+so[i];}
-		else if(dau=='-'){gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];kq=kq-so[i];}
-		else if(dau=='*'){gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];kq=kq*so[i];}
-		else if(dau=='/'){gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];kq=kq/so[i];}
-		else if(dau=='^'){int somu;gotoxy(3,3);cin>>somu;i--;gotoxy(3,3);cout<<"   ";so[i]=pow(so[i],somu);}
+		if(dau=='+')	 {gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];}
+		else if(dau=='-'){gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];}
+		else if(dau=='*'){gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];}
+		else if(dau=='/'){gotoxy(11,1);cout<<"            ";gotoxy(6,1);cout<<i+1<<"] = ";cin>>so[i];}
+		else if(dau=='^'){
+			i--;int somu;gotoxy(3,3);cin>>somu;gotoxy(3,3);cout<<"   ";
+			so[i]=pow(so[i],somu);
+		}
 		else if(dau=='V'||dau=='v'){
-			i--;int bac;
-			gotoxy(4,2);cin>>bac;
-			so[i]=pow(so[i],1/bac);
+			i--;int bac;gotoxy(3,3);cin>>bac;gotoxy(3,3);cout<<"   ";bac=1/bac;
+			so[i]=pow(so[i],bac);
 		}
 		else if(dau=='L'||dau=='l'){
 			i--;
@@ -86,6 +88,13 @@ void NormalMath(){
 			so[i]=so[i]/100;
 		}
 		else if(dau=='='){
+			kq=so[0];
+			for(int j=0;j<i-1;j++){
+				if(d[j]==43)kq=kq+so[i+1];
+				else if(d[j]==45)kq=kq-so[i+1];
+				else if(d[j]==42)kq=kq*so[i+1];
+				else if(d[j]==47)kq=kq/so[i+1];
+			}
 			gotoxy(5,2);cout<<kq;
 			getche();
 			break;
@@ -95,12 +104,12 @@ void NormalMath(){
 void TTPhanSo(){	
 	system("cls");
 	system("color f2");
-	double tu[255],mau[255];char dau;
+	int tu[255],mau[255];char dau;
 	gotoxy(1,3);cout<<"X [1]";
 	gotoxy(7,3);cout<<"-------";
 	gotoxy(7,2);cin>>tu[0];
 	gotoxy(7,4);cin>>mau[0];
-	double TT=tu[0],TM=mau[0];
+	int TT=tu[0],TM=mau[0];
 	for(int i=1;i<255;i++)
 	{
 		gotoxy(1,3);cout<<"X ["<<i+1<<"]";
@@ -137,6 +146,9 @@ void TTPhanSo(){
 		}
 		else if(dau=='=')
 		{
+			for(int j=(TT+TM)/2;j>=0;j--){
+				if(TT%j==0&&TM%j==0){TT=TT/j;TM=TM/j;break;}
+			}
 			gotoxy(17,3);cout<<"-------";
 			gotoxy(17,2);cout<<TT;
 			gotoxy(17,4);cout<<TM;
@@ -144,54 +156,6 @@ void TTPhanSo(){
 			break;
 		}
 	}
-}
-void RutGon(){
-	system("cls");
-	system("color f5");
-	int tu,mau;
-	gotoxy(2,3);cout<<"-----";
-	gotoxy(2,2);cin>>tu;
-	gotoxy(2,4);cin>>mau;
-	gotoxy(8,3);cout<<"= -----";
-	for(int i=(tu+mau)/2;i>=0;i--)
-	{
-		if(tu%i==0&&mau%i==0){tu=tu/i;mau=mau/i;break;}
-	}
-	gotoxy(10,2);cout<<tu;
-	gotoxy(10,4);cout<<mau;
-	getche();
-}
-void MHPS(){
-	while(1)
-	{
-		system("cls");
-		system("color f0");
-		gotoxy(3,3);cout<<"--------------";
-		gotoxy(3,4);cout<<"| CALCULATOR |";
-		gotoxy(3,5);cout<<"--------------";
-		gotoxy(2,7);cout<<"1. Calculate";
-		gotoxy(2,8);cout<<"2. Compact";
-		char a=getche();
-		if(a=='1')TTPhanSo();
-		else if(a=='2')RutGon();
-		else if(a==27)break;
-	}
-}
-void Math(){
-	do
-	{
-		system("cls");
-		system("color f0");
-		gotoxy(3,3);cout<<"--------------";
-		gotoxy(3,4);cout<<"| CALCULATOR |";
-		gotoxy(3,5);cout<<"--------------";
-		gotoxy(2,7);cout<<"1. Real number";
-		gotoxy(2,8);cout<<"2. Fraction";
-		char a=getche();
-		if(a=='1') NormalMath();
-		else if(a=='2') MHPS();
-		else if(a==27)break;
-	}while(1);
 }
 void PhuongTrinhBac2(){
 	system("cls");
@@ -223,72 +187,41 @@ void PhuongTrinhBac2(){
     getche();
 }
 void PhuongTrinhBac3(){
-	system("cls");
-	system("color f8");
-	double a,b,c,d,delta,k,kq;
-    cout<<"Import a: ";cin>>a;
-    cout<<"Import b: ";cin>>b;
-    cout<<"Import c: ";cin>>c;
-    cout<<"Import d: ";cin>>d;
-    delta=pow(b,2)-3*a*c;
-    k=(9*a*b*c-2*pow(b,3)-27*pow(a,2)*d)/(sqrt(pow(abs(delta),3)));
-    cout<<"         2"<<endl;
-    cout<<"Delta = b  - 3ac = "<<delta<<endl;
-    cout<<"k = "<<k<<endl;
-    if(a!=0){
-    	cout<<"  3     2\n";
-    	cout<<"ax  + bx  + cx + d\n";
-    	cout<<"  3    2\n";
-		cout<<a<<"x ";
-		if(b<0)cout<<"- "<<abs(b)<<"x ";
-        else cout<<"+ "<<b<<"x ";
-        if(c<0)cout<<"- "<<abs(c)<<"x";
-        else cout<<"+ "<<abs(c)<<"x";
-        if(d<0)cout<<"- "<<abs(d);
-        else cout<<"+ "<<abs(d);
-    	if(delta<0)cout<<"The equation has a solution: x = "<<((sqrt(abs(delta)))/(3*a))*(pow(k+sqrt(pow(k,2)+1),1/3)+pow(k-sqrt(pow(k,2)+1),1/3))-b/(3*a)<<endl;
-		else if(delta==0)cout<<"The equation with multiple solutions: x = "<<(-b+pow(pow(b,3)-27*pow(a,2)*d,1/3))/(3*a)<<endl;
-		else if(delta>0){
-			if(abs(k)<=1){
-				cout<<"The equation has 3 solutions: x1 = "<<(2*sqrt(delta)*cos(acos(k)/3)-b)/(3*a)<<endl;
-				cout<<"                              x2 = "<<(2*sqrt(delta)*cos((acos(k)/3)-(2*M_PI/3))-b)/(3*a)<<endl;
-				cout<<"                              x3 = "<<(2*sqrt(delta)*cos((acos(k)/3)+(2*M_PI/3))-b)/(3*a)<<endl;
-			}
-			else if(abs(k)>1)cout<<"The equation has a solution: x = ";cout<<((sqrt(delta)*abs(k))/(3*a))*(pow(abs(k)+sqrt(pow(k,2)-1),1/3)+(pow(abs(k)-sqrt(pow(k,2)-1),1/3))-b/(3*a))<<endl;
-		}
-	}
-	getche();
-}
-void MHPhuongTrinh(){
-	do
-	{
-		system("cls");
-		system("color f0");
-		gotoxy(3,3);cout<<"--------------";
-		gotoxy(3,4);cout<<"| CALCULATOR |";
-		gotoxy(3,5);cout<<"--------------";
-		gotoxy(2,7);cout<<"Choose equation 2 ~ 3";
-		char a=getche();
-		if(a=='2') PhuongTrinhBac2();
-		else if(a=='3') PhuongTrinhBac3();
-		else if(a==27)break;
-	}while(1);
-}
-void MHToanHoc(){
-	do
-	{
-		system("cls");
-		system("color f0");
-		gotoxy(3,3);cout<<"--------------";
-		gotoxy(3,4);cout<<"| CALCULATOR |";
-		gotoxy(3,5);cout<<"--------------";
-		gotoxy(2,7);cout<<"1. Normal calculate";
-		gotoxy(2,8);cout<<"2. Equation";
-		char a=getche();
-		if(a=='1') Math();
-		else if(a=='2') MHPhuongTrinh();
-		else if(a==27)break;
-	}while(1);
+//	system("cls");
+//	system("color f8");
+//	double a,b,c,d,delta,k,kq;
+//    cout<<"Import a: ";cin>>a;
+//    cout<<"Import b: ";cin>>b;
+//    cout<<"Import c: ";cin>>c;
+//    cout<<"Import d: ";cin>>d;
+//    delta=pow(b,2)-3*a*c;
+//    k=(9*a*b*c-2*pow(b,3)-27*pow(a,2)*d)/(sqrt(pow(abs(delta),3)));
+//    cout<<"         2"<<endl;
+//    cout<<"Delta = b  - 3ac = "<<delta<<endl;
+//    cout<<"k = "<<k<<endl;
+//    if(a!=0){
+//    	cout<<"  3     2\n";
+//    	cout<<"ax  + bx  + cx + d\n";
+//    	cout<<"  3    2\n";
+//		cout<<a<<"x ";
+//		if(b<0)cout<<"- "<<abs(b)<<"x ";
+//        else cout<<"+ "<<b<<"x ";
+//        if(c<0)cout<<"- "<<abs(c)<<"x";
+//        else cout<<"+ "<<abs(c)<<"x";
+//        if(d<0)cout<<"- "<<abs(d);
+//        else cout<<"+ "<<abs(d);
+//    	if(delta<0)cout<<"The equation has a solution: x = "<<((sqrt(abs(delta)))/(3*a))*(pow(k+sqrt(pow(k,2)+1),1/3)+pow(k-sqrt(pow(k,2)+1),1/3))-b/(3*a)<<endl;
+//		else if(delta==0)cout<<"The equation with multiple solutions: x = "<<(-b+pow(pow(b,3)-27*pow(a,2)*d,1/3))/(3*a)<<endl;
+//		else if(delta>0){
+//			if(abs(k)<=1){
+//				cout<<"The equation has 3 solutions: x1 = "<<(2*sqrt(delta)*cos(acos(k)/3)-b)/(3*a)<<endl;
+//				cout<<"                              x2 = "<<(2*sqrt(delta)*cos((acos(k)/3)-(2*M_PI/3))-b)/(3*a)<<endl;
+//				cout<<"                              x3 = "<<(2*sqrt(delta)*cos((acos(k)/3)+(2*M_PI/3))-b)/(3*a)<<endl;
+//			}
+//			else if(abs(k)>1)cout<<"The equation has a solution: x = ";cout<<((sqrt(delta)*abs(k))/(3*a))*(pow(abs(k)+sqrt(pow(k,2)-1),1/3)+(pow(abs(k)-sqrt(pow(k,2)-1),1/3))-b/(3*a))<<endl;
+//		}
+//	}
+//	getche();
 }
 void Game1(){
 	system("cls");
@@ -319,20 +252,6 @@ void Game1(){
 	}
 	getche();
 }
-void MHMinigames(){
-	do
-	{
-		system("cls");
-		system("color f0");
-		gotoxy(3,3);cout<<"--------------";
-		gotoxy(3,4);cout<<"| Mini games |";
-		gotoxy(3,5);cout<<"--------------";
-		gotoxy(2,7);cout<<"1. Number guessing game";
-		char a=getche();
-		if(a=='1') Game1();
-		else if(a==27)break;
-	}while(1);
-}
 int main(){
 	do
 	{
@@ -341,14 +260,18 @@ int main(){
 		gotoxy(3,3);cout<<"-----------------";
 		gotoxy(3,4);cout<<"| Program study |";
 		gotoxy(3,5);cout<<"-----------------";
-		gotoxy(2,7);cout<<"1. Calculator";
-		gotoxy(2,8);cout<<"2. Mini games";
-		gotoxy(2,9);cout<<"3. Code";
-		gotoxy(2,10);cout<<"4. Exit";
+		gotoxy(2,7);cout<<"1. Calculate real numbers";
+		gotoxy(2,8);cout<<"2. Calculate fractions";
+		gotoxy(2,9);cout<<"3. Solving quadratic equations";
+		gotoxy(2,10);cout<<"4. Mini games - Guess number game";
+		gotoxy(2,11);cout<<"5. Code";
+		gotoxy(2,12);cout<<"6. Exit";
 		char a=getche();
-		if(a=='1') MHToanHoc();
-		else if(a=='2') MHMinigames();
-		else if(a=='3'){
+		if(a=='1') NormalMath();
+		else if(a=='2') TTPhanSo();
+		else if(a=='3') PhuongTrinhBac2();
+		else if(a=='4') Game1();
+		else if(a=='5'){
 			system("cls");
 			system("color b5");
 			gotoxy(4,3);cout<<"Copy link";
@@ -356,12 +279,12 @@ int main(){
 			gotoxy(8,5);cout<<"|";
 			gotoxy(8,6);cout<<"|";
 			gotoxy(8,7);cout<<"V";
-			gotoxy(1,8);cout<<"https://github.com/HuongHackerDepTrai/HuongHackerDepTrai/blob/main/Calculator.cpp";
+			gotoxy(1,8);cout<<"https://github.com/HuongHackerDepTrai/HuongHackerDepTrai/blob/main/CalculatorBHN.cpp";
 			gotoxy(4,9);cout<<"App: DEV C/C++    Link download:https://sourceforge.net/projects/orwelldevcpp/";
 			gotoxy(4,10);cout<<"Language: C++";
 			getche();
 		}
-		else if(a=='4')exit(1);
+		else if(a=='6')exit(1);
 	}while(1);
     return 0;
 }
